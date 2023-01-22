03dfd0bcbeec3f04f21b6fa42bd51c0a44f9a807
Marvalyn <111514523+Marvalyn@users.noreply.github.com>
2023-01-22 18:18:17 +0000

    changes to checktime function

--- starter/script.js
+++ starter/script.js
@@ -12,33 +12,6 @@ var textField = $(".input-group");
 var saveBtn = $('<button class="saveBtn"><i class="fas fa-save"></i></button>');
 // textField.append('<button class="saveBtn"><i class="fas fa-save"></i></button>');
 textField.append(saveBtn);
-//code below will be useful when coding save  button 
-// function handleFormSubmit(event) {
-//     event.preventDefault();
-  
-//     var shoppingItem = $('input[name="shopping-input"]').val();
-  
-//     if (!shoppingItem) {
-//       console.log('No shopping item filled out in form!');
-//       return;
-//     }
-  
-//     var shoppingListItemEl = $(
-//       '<li class="flex-row justify-space-between align-center p-2 bg-light text-dark">'
-//     );
-//     shoppingListItemEl.text(shoppingItem);
-  
-//     // add delete button to remove shopping list item
-//     shoppingListItemEl.append(
-//       '<button class="btn btn-danger btn-small delete-item-btn">Remove</button>'
-//     );
-  
-//     // print to the page
-//     shoppingListEl.append(shoppingListItemEl);
-  
-//     // clear the form input element
-//     $('input[name="shopping-input"]').val('');
-//   }
 
 //add get & set JSON to add info to client side storage so info stays when page is refreshed
 // localStorage.setItem("activity");
@@ -46,18 +19,28 @@ textField.append(saveBtn);
 
 
 function checkTime() {
-    var currentTime = moment().format("HH:mm:ss");
-    var scheduleTime = $("textarea").attr("id");
+    var currentTime = moment().format("HH");
+    var scheduleTime = parseInt($("textarea").attr("id"));
+    // var scheduleTime = $("span").val();
     console.log(currentTime);
     console.log(scheduleTime);
 
+    // $("textarea").each(function() {
+        
+    //     if (scheduleTime = currentTime) {
+    //         $(this).addClass("present");
+    //     } else if (scheduleTime < currentTime) {
+    //         $(this).addClass("past");
+    //     } else (scheduleTime > currentTime)
+    //             $(this).addClass("future");
+    // });
     $("textarea").each(function() {
-        if (scheduleTime = currentTime) {
-            $(this).addClass("present");
-        } else if (scheduleTime < currentTime) {
+        if (scheduleTime < currentTime) {
             $(this).addClass("past");
+        } else if (scheduleTime == currentTime) {
+            $(this).addClass("present");
         } else (scheduleTime > currentTime) 
-                $(this).addClass("future");
+            $(this).addClass("future");
     });
 
 };
